c73fb840e05cb2e263e48de78325d1f7f0c79480
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-05-18 19:10:28 +0500

    Add some js code, .gitignore

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+
+	let receivedText = 0;
+
+	$.getJSON(
+		'https://api.jsonbin.io/b/5e905926172eb643896166e7',
+		function(data) {
+			receivedText = data.text;
+		}
+	);
+
+	$('.btn-create-text').click(function() {
+		for (let i = 0; i < receivedText.length; i++) {
+			console.log(receivedText[i]);
+		}
+	});
+
+});
 
