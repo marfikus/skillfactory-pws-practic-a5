616bceec1ee9a32f2af87db4b00a726e6dd622ed
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-05-19 21:55:56 +0500

    Add comments, actions on button click

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-function checkRepeatField(field) {
-
-}
 
 $(document).ready(function() {
 
-	let receivedText = 0;
+	let receivedText = 0; // 
+	let fields = []; // общий массив найденных полей
 
 	$.getJSON(
 		"https://api.jsonbin.io/b/5e905926172eb643896166e7",
 		function(data) {
-			// receivedText = data.text;
+			receivedText = data.text;
 
-			let fields = [];
-			let curStrFields = 0;
+			let curStrFields = 0; // поля найденные в текущей строке
 
+			// бежим по элементам (строкам) полученного массива,
+			// ищем все поля для заполнения:
 			for (let str of data.text) {
 				// const regexp = new RegExp("\{+\}", "ig");
 				// curStrFields = receivedText[0].match(/{(.[^{}]+)}/ig);
 				curStrFields = str.match(/{(.[^{}]+)}/ig);
 				console.log(curStrFields);
 
+				// если результат есть:
 				if (curStrFields !== null) {
 					console.log(curStrFields.length);
 
+					// добавляем найденные поля в общий массив,
+					// попутно фильтруя повторяющиеся поля:
 					for (let curStrField of curStrFields) {
 						let repeatField = false;
 						for (let field of fields) {
@@ -32,9 +34,11 @@ $(document).ready(function() {
 							}
 						}
 
+						// если поле уникально, то добавляем его в общий массив:
 						if (repeatField === false) {
 							fields.push(curStrField);
 
+							// ну и создаём поле в форме:
 							let fieldName = curStrField.substr(1, curStrField.length-2);
 							$(".data-inputs").append(`<input type="text" name="${fieldName}" placeholder="${fieldName}">`);
 						}
@@ -42,38 +46,26 @@ $(document).ready(function() {
 					// fields = fields.concat(curStrFields);
 				}
 			}
-
 			console.log(fields.length);
 			console.log(fields);
-
-			let resultText = data.text.join(' ');
-			console.log(resultText);
-			for (let field of fields) {
-				let fieldName = field.substr(1, field.length-2);
-				let fiedlValue = $(`input[name=${fieldName}]`).val();
-				console.log("fiedlValue:", fiedlValue);
-				resultText = resultText.replace(new RegExp(field,'g'), fiedlValue);
-			}
-			console.log(resultText);
-
-			$('.result').text(resultText);
-			
-
-			// let str = receivedText[0];
-			// let results = str.matchAll(/{(.[^{}]+)}/i);
-			// results = Array.from(results);
-			// console.log(results);
 		}
 	);
 
-
-
 	$(".btn-create-text").click(function() {
-		// for (let i = 0; i < receivedText.length; i++) {
-			// console.log(receivedText[0].match);
-		// }
-		console.log(receivedText);
-	});
+		// собираем строки принятого массива в общий текст:
+		let resultText = receivedText.join(' ');
+		console.log(resultText);
 
+		// и заменяем поля текста на значения из полей формы:
+		for (let field of fields) {
+			let fieldName = field.substr(1, field.length-2);
+			let fiedlValue = $(`input[name=${fieldName}]`).val();
+			console.log("fiedlValue:", fiedlValue);
+			resultText = resultText.replace(new RegExp(field,'g'), fiedlValue);
+		}
+		console.log(resultText);
+
+		$('.result').text(resultText);
+	});
 });
 
